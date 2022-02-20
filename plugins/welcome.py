c67302294c4eeea931d9fb12fd714ffd72f59df8
sherlocksha1 <98747957+sherlocksha1@users.noreply.github.com>
2022-02-20 09:32:19 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message, User
 
 
 
-@Client.on_message(filters.new_chat_members)
+@Client.off_message(filters.new_chat_members)
 async def welcome(bot,message):
 	chatid= message.chat.id
 	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
