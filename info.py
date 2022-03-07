850520415c50163aa6f1717ac53d287a71a9e112
sherlocksha1 <98747957+sherlocksha1@users.noreply.github.com>
2022-03-07 17:07:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,6 @@ BOT_TOKEN = environ['BOT_TOKEN']
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', '')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
